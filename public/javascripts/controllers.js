e0a4532e3e00608dfebe7d4a22069720083a83e7
nlintz <nlintz@gmail.com>
2013-09-18 22:29:15 -0400

    added lorem ipsum test text

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -8,12 +8,12 @@ angular.module('teamCinnamon.controllers', []).
 			},
 			{
 				name: "Sharon Grimshaw",
-				description: "Sharon designs cool looking stuff",
+				description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
 				imgURL: "sharon.jpg"
 			},
 			{
 				name: "Chaz Gwenapp",
-				description: "Chaz knows all about AI",
+				description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
 				imgURL: "chaz.jpg"
 			},
 			{
