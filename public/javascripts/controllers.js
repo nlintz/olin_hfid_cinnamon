3eca6bd9a31c0b59606fa39218babd0838461a26
nlintz <nlintz@gmail.com>
2013-09-20 09:06:24 -0400

    added chazs info

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -13,7 +13,7 @@ angular.module('teamCinnamon.controllers', []).
 			},
 			{
 				name: "Chaz Gwenapp",
-				description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+				description: "Chaz is a Junior working on an E:C major with a focus on AI. In his free time, he enjoys hanging out with friends, playing the organ, and working on crazy CS projects (though not typically at the same time).",
 				imgURL: "chaz.jpg"
 			},
 			{
