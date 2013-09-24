e045c01be356bf6a86c850108ce257074045611c
nlintz <nlintz@gmail.com>
2013-09-24 12:00:47 -0400

    moved team logic to json file

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,7 +4,8 @@ angular.module('teamCinnamon', ['ngSanitize', 'teamCinnamon.controllers', 'teamC
 	      when('/home', {templateUrl: 'templates/home.html'}).
 	      when('/process', {templateUrl: 'templates/process.html'}).
 	      when('/proposal', {templateUrl: 'templates/proposal.html'}).
-	      when('/team', {templateUrl: 'templates/team.html', controller: 'teamController'}).
+	      when('/team', 
+	      	{templateUrl: 'templates/team.html', controller: 'teamController', resolve: {Team: 'Team'}}).
 	      when('/inspirational_designs', 
 	      	{templateUrl: 'templates/inspirational_designs.html', controller: 'inspirationalDesignsController', resolve: {InspirationalDesigns: 'InspirationalDesign'}}).
 	      otherwise({redirectTo: '/home'});
