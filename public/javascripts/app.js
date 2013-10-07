c6dd619a07974e26bc96d15f8d480f991a6b38fb
Nathan Lintz <nlintz@gmail.com>
2013-10-07 01:09:48 -0700

    added story boards and interaction narrative

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,20 @@
 angular.module('teamCinnamon', ['ngSanitize', 'teamCinnamon.controllers', 'teamCinnamon.services', 'teamCinnamon.directives', 'teamCinnamon.filters']).
 	config(['$routeProvider', function($routeProvider) {
 	  $routeProvider.
-	      when('/home', {templateUrl: 'templates/home.html'}).
-	      when('/process', {templateUrl: 'templates/process.html'}).
-	      when('/proposal', {templateUrl: 'templates/proposal.html'}).
-	      when('/team', 
-	      	{templateUrl: 'templates/team.html', controller: 'teamController', resolve: {Team: 'Team'}}).
-	      when('/inspirational_designs', 
-	      	{templateUrl: 'templates/inspirational_designs.html', controller: 'inspirationalDesignsController', resolve: {InspirationalDesigns: 'InspirationalDesign'}}).
-	      when('/needs_analysis', 
-	      	{templateUrl: 'templates/needs_analysis.html', controller:'needsAnalysisController', resolve: {NeedsAnalysis: 'NeedsAnalysis', Personas: 'Personas'}}).
-	      when('/work_distribution',
-	      	{templateUrl: 'templates/work_distribution.html', controller: 'workDistributionController', resolve: {WorkDistribution: 'WorkDistribution'}}).
-	      otherwise({redirectTo: '/home'});
+		  when('/home', {templateUrl: 'templates/home.html'}).
+		  when('/process', {templateUrl: 'templates/process.html'}).
+		  when('/proposal', {templateUrl: 'templates/proposal.html'}).
+		  when('/team', 
+			{templateUrl: 'templates/team.html', controller: 'teamController', resolve: {Team: 'Team'}}).
+		  when('/inspirational_designs', 
+			{templateUrl: 'templates/inspirational_designs.html', controller: 'inspirationalDesignsController', resolve: {InspirationalDesigns: 'InspirationalDesign'}}).
+		  when('/needs_analysis', 
+			{templateUrl: 'templates/needs_analysis.html', controller:'needsAnalysisController', resolve: {NeedsAnalysis: 'NeedsAnalysis', Personas: 'Personas'}}).
+		  when('/work_distribution',
+			{templateUrl: 'templates/work_distribution.html', controller: 'workDistributionController', resolve: {WorkDistribution: 'WorkDistribution'}}).
+		  when('/story_boards',
+			{templateUrl: 'templates/story_boards.html'}).
+		  when('/interaction_flows',
+			{templateUrl: 'templates/interaction_flows.html'}).
+		  otherwise({redirectTo: '/home'});
 }]);
